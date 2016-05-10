584258b1e3767d200e32c183c80f6de0132aff36
Nikola Kocić <nikolakocic@gmail.com>
2016-05-10 19:44:56 +0200

    Get current tab

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate i3ipc;
 use i3ipc::I3Connection;
-use i3ipc::reply::Node;
+use i3ipc::reply::{Node, NodeType};
 
 fn get_nodes<'a, F>(tree: &'a Node, condition: &F) -> Option<Vec<&'a Node>>
     where F: Fn(&Node) -> bool
@@ -18,10 +18,23 @@ fn get_nodes<'a, F>(tree: &'a Node, condition: &F) -> Option<Vec<&'a Node>>
     return None;
 }
 
-fn superfocus(c: &mut I3Connection, direction: &str) {}
+fn get_current_tab<'a>(nodes: &[&'a Node]) -> &'a Node {
+    let i = nodes.iter().position(|&n| {
+        match n.nodetype { NodeType::Con => false, _ => true }
+    }).unwrap();
+    // i is Workspace
+    // i - 1 is "Root container" for workspace tabs
+    // i - 2 is current tab
+    nodes[i - 2]
+}
+
+fn superfocus(c: &mut I3Connection, direction: &str) {
+    let tree = c.get_tree().unwrap();
+    let nodes = get_nodes(&tree, &|n: &Node| { return n.focused; }).unwrap();
+    let current_tab = get_current_tab(&nodes);
+}
 
 fn main() {
     let mut connection = I3Connection::connect().unwrap();
-    let tree = connection.get_tree().unwrap();
-    let nodes = get_nodes(&tree, &|n: &Node| { return n.focused; }).unwrap();
+    superfocus(&mut connection, "right");
 }
